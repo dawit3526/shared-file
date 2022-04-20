4200a535fa13df6425940d41b75130493dd46a87
dawit3526 <dawit3526@yahoo.com>
2022-04-20 14:30:49 -0500

    Create getDayOfWeek.groovy

---
+++ getDayOfWeek.groovy
@@ -0,0 +1,9 @@
+
+def call() {
+ def now = new Date()
+	echo now.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
+	
+	Calendar calendar = Calendar.getInstance();
+	day = now[calendar.DAY_OF_WEEK]
+	return day.toString()   
+}
